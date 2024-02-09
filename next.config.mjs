9857bc50bde758c0dc1d2d06c2613083fc2d5e5a
Antonio Espinoza <aespinozareyes9@gmail.com>
2024-02-09 09:35:16 -0300

    add dockerfile

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'standalone', //Recomendado cuando se crean imagenes de docker
+};
 
 export default nextConfig;
